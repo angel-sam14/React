1c391c483dab19ae66f3052434cf49ee21f910de
angel-sam14 <angelannasam14@gmail.com>
2026-09-17 08:56:38 -0700

    Added sample1 programs

---
+++ sample1/src/App.jsx
@@ -0,0 +1,47 @@
+// import React from "react";
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1>Hello, World!</h1>
+//       </div>
+//     );
+//   }
+// }
+// export default App;
+// -----------------------------------------------
+// REACT COMPONENTS
+// -----------------
+
+// 1. Functional Component
+// -----------------------
+
+// import React from "react";  
+// import Student from "./student";
+
+// function App() {
+//   return (
+//     <div>
+//           <h2>Student Details</h2>
+//       { /* <h1>Name: Angel Sam</h1>
+//       <h1>Course: Python Full Stack Development</h1> */ }
+//       <Student/>
+//     </div>
+//   );
+// }
+
+// export default App;
+// -----------------------------------------------
+
+// function App() {
+//   return (
+//     <div>
+//           <h2>Student Details</h2>
+//       <Student name="Angel Sam" age={20} course="Python Full Stack Development" />
+//     </div>
+//   );
+// }
+
+// export default App;
+// --------------------------------------------------
---
+++ sample1/src/Student.jsx
@@ -0,0 +1,22 @@
+// function Student() {
+//   return (
+//     <div>
+//       <h1>Phone number:8281696025</h1>
+//       <h1>Address:kochi</h1>
+//     </div>
+//   );
+// }
+
+// export default Student;
+// ---------------------------
+
+// function Student(props) {
+//   return (
+//     <div>
+//       <h1>Name: {props.name}</h1>
+//        <h1>age: {props.age}</h1>
+//       <h1>Course: {props.course}</h1>
+//     </div>
+//   );
+// }
+// export default Student;
